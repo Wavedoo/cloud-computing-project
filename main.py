36e7bb00bf2f4b3977c71575ef8d534a4a0ea93a
Sterling <sterling1@hotmail.ca>
2024-04-02 15:54:54 -0400

    Main file update

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import threading
 from tkinter import *
 import json;
 import io;
+import warnings
 import os
 from google.cloud import pubsub_v1      #pip install google-cloud-pubsub
 
@@ -18,23 +19,35 @@ publisher = pubsub_v1.PublisherClient()
 subscriber = pubsub_v1.SubscriberClient()
 subscription_path = subscriber.subscription_path(project_id, subscription_id)
 
+# Global Variables
 canvas = None
-outputLabel = None
+outputLabel1 = None
+outputLabel2 = None
 window = None
 trackField = None
 timeField = None
 
 def callback(message: pubsub_v1.subscriber.message.Message) -> None:
     data = json.loads(message.data)
-    window.after(0, lambda: changeOutputLabelText(data))
+    print(f"Received {data}.")
+    window.after(0, lambda: changeOutputLabelText(str(data['east']), str(data['west'])))
     message.ack()
 
+def WaitForInformation():
+    streaming_pull_future = subscriber.subscribe(subscription_path, callback=callback)
+    with subscriber:
+        try:
+            streaming_pull_future.result()
+        except KeyboardInterrupt:
+            streaming_pull_future.cancel(await_msg_callbacks=True)  # blocks until done
 
-def changeOutputLabelText(newOutputLabelText):
+def changeOutputLabelText(newOutputLabel1Text, newOutputLabel2Text):
     # Reference Global Variables
-    global outputLabel
+    global outputLabel1, outputLabel2
 
-    canvas.itemconfig(outputLabel, text=newOutputLabelText, fill="black")  # Change text to the selected director
+    canvas.itemconfig(outputLabel1, text=newOutputLabel1Text, fill="black")  # Change text to the selected director
+    canvas.itemconfig(outputLabel2, text=newOutputLabel2Text, fill="black")  # Change text to the selected director
+
     print("Changed Output Text")
 
 
@@ -51,19 +64,33 @@ def submitInformation():
     message['track'] = min(int(trackNumber), 900) #Limits choices to 1-900
     future = publisher.publish(topic_path, json.dumps(message).encode('utf-8'))
 
-    streaming_pull_future2 = subscriber.subscribe(subscription_path, callback=callback)
-    with subscriber:
-        try:
-            streaming_pull_future2.result()
-        except KeyboardInterrupt:
-            streaming_pull_future2.cancel(await_msg_callbacks=True)  # blocks until done
+    # WaitForInformation()
+    # streaming_pull_future2 = subscriber.subscribe(subscription_path, callback=callback)
+    # with subscriber:
+    #     try:
+    #         streaming_pull_future2.result()
+    #     except RuntimeError:
+    #         warnings.warn(
+    #             "Scheduling a callback after executor shutdown.",
+    #            category=RuntimeWarning,                
+    #            stacklevel=2,
+    #         )
+        
+
+    # streaming_pull_future2 = subscriber.subscribe(subscription_path, callback=callback)
+    # with subscriber:
+    #     try:
+    #         streaming_pull_future2.result()
+    #     except KeyboardInterrupt:
+    #         streaming_pull_future2.cancel(await_msg_callbacks=True)  # blocks until done
 
 
 
 def createWindow():
     # Reference Global Variables
     global canvas
-    global outputLabel
+    global outputLabel1
+    global outputLabel2
     global window
     global trackField
     global timeField
@@ -83,6 +110,10 @@ def createWindow():
     canvas.pack()  # Applying the GUI window properties
 
     # Initializing GUI Elements
+    # --Background Image
+    backgroundImage = PhotoImage(file='Background.png')  # Initializing the background image to use
+    canvas.create_image(250, 350, image=backgroundImage)  # Centering the background image in the window
+
     # --Track Field
     trackField = Entry(window, width=50)  # Creating the link field where the user will enter the track number
     canvas.create_text(250, 170, text="Enter Track: ",
@@ -94,13 +125,15 @@ def createWindow():
                        font=('Arial bold', 15))  # Text label above the link field
 
     # --Preview Button
-    submitButtonImage = PhotoImage(file="")
-    submitButton = Button(window, command=submitInformation,
-                          borderwidth=20)  # Creating the submit button which users click to send the information
+    submitButtonImage = PhotoImage(file="SubmitButton.png")
+    submitButton = Button(window, image=submitButtonImage, command=submitInformation,
+                          borderwidth=0)  # Creating the submit button which users click to send the information
 
-    outputLabel = canvas.create_text(250, 500, text="Output",
-                                     font=('Arial bold', 15))  # Text label above
-    # the select button
+    outputLabel1 = canvas.create_text(250, 500, text="",
+                                      font=('Arial bold', 15))  # Text label above the select button
+
+    outputLabel2 = canvas.create_text(250, 560, text="",
+                                      font=('Arial bold', 15))  # Text label above the select button
 
     # --Adding GUI Elements to the Window
     canvas.create_window(250, 220, window=trackField)  # Centering the link field in the window
@@ -112,4 +145,10 @@ def createWindow():
 
 uiThread = threading.Thread(target=createWindow)
 uiThread.start()
+streaming_pull_future2 = subscriber.subscribe(subscription_path, callback=callback)
+with subscriber:
+    try:
+        streaming_pull_future2.result()
+    except KeyboardInterrupt:
+        streaming_pull_future2.cancel(await_msg_callbacks=True)  # blocks until done
 
